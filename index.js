8d6b9b8080f2ee41ce0959509b5d8413c0f4488d
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2020-05-23 16:46:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ message.channel.send(`erreur`)
 
 
 if(message.content.startsWith("!(S)MP")) {
-    if("429548231219740672".includes(message.author.id)){
+    if("234368202379886593".includes(message.author.id)){
     if(message.mentions.users.first()){
 
     let messageToSend = message.content.split(" ").slice(2).join(" ");
